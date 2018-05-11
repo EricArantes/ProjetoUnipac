143e4c41b6049ebc0e2243374fd4ee154b6211d3
Eric Arantes <ericarantes0@gmail.com>
2018-05-11 16:41:35 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿ $(window).load(function () {      
+        $(".loader").delay(1500).fadeOut("slow"); //retire o delay quando for copiar!       
+       
+        $("#tudo_page").toggle("fast");
+        });
+
+        $(document).ready(function() {
+            // Exibe ou oculta o botão
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 200) {
+                    $('.voltar-ao-topo').fadeIn(200);
+                } else {
+                    $('.voltar-ao-topo').fadeOut(200);
+                }
+            });
+            
+            // Faz animação para subir
+            $('.voltar-ao-topo').click(function(event) {
+                event.preventDefault();
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
